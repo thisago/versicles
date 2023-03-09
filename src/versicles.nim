d87264e7eef3348ec0b7affc75f9d7fb69460844
Thiago Navarro <thiago@oxyoy.com>
2023-03-09 22:30:00 +0000

    Fixed stripping underline

--- src/versicles.nim
+++ src/versicles.nim
@@ -88,7 +88,7 @@ proc parseList(list, outJson: string; saveAllLines = false): bool =
     var verses: seq[string]
     for parenthesis in line.getAllFirstLevelParenthesis:
       for verse in parenthesis.strip.findAll(verseRegex):
-        verses.add verse.strip(NonExtendedAlphanumeric, [':', ',', '-', ' ']).
+        verses.add verse.strip(NonExtendedAlphanumeric, [':', ',', '-', ' ', '_']).
           strip(chars = AllChars - Letters - Digits)
     if verses.len > 0 or saveAllLines:
       node.add %*{
